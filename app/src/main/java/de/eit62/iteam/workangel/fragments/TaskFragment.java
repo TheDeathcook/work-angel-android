d1ffd2b61f60300f6b2f5a977ced2da336ccbf5e
TheDeathcook <lukas.tegethoff2@gmail.com>
2018-06-17 23:54:15 +0200

    Implemented the web service call as an AsyncTask. Android does not allow network activity in the main thread so that it does not block the UIThread, so the web service calls have to be implemented as AsyncTasks. AsyncTasks are bound to the current Activity, so if a configuration change occurs in the Activity which causes it to be destroyed (such as an orientation change), the web service call would be interrupted as well. To prevent this, the Task is encapsulated in a Fragment which is retained across configuration changes and can be attached to different Activities. Communication between the Activity and the Fragment/Task are handled by the Activity implementing the Interface TaskCallback.

---
+++ app/src/main/java/de/eit62/iteam/workangel/fragments/TaskFragment.java
@@ -0,0 +1,90 @@
+package de.eit62.iteam.workangel.fragments;
+
+import android.app.Fragment;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+import de.eit62.iteam.workangel.beans.AppUser;
+import de.eit62.iteam.workangel.webservice.WAServiceClient;
+
+public class TaskFragment extends Fragment {
+	
+	private TaskCallback callbacks;
+	private UserLoginTask loginTask;
+	
+	@Override
+	public void onAttach(Context context) {
+		super.onAttach(context);
+		callbacks = (TaskCallback) context;
+	}
+	
+	@Override
+	public void onCreate(@Nullable Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setRetainInstance(true);
+	}
+	
+	public void executeLogin(String username, char[] password) {
+		loginTask = new UserLoginTask();
+		loginTask.execute(username, password);
+	}
+	
+	@Override
+	public void onDetach() {
+		super.onDetach();
+		callbacks = null;
+	}
+	
+	public interface TaskCallback<Progress, Result> {
+		
+		void onPreExecute();
+		
+		void onProgressUpdate(Progress progress);
+		
+		void onCancelled();
+		
+		void onPostExecute(Result result);
+	}
+	
+	private class UserLoginTask extends AsyncTask<Object, Void, AppUser> {
+		
+		private final WAServiceClient serviceClient = WAServiceClient.getInstance();
+		private String username;
+		private char[] password;
+		
+		@Override
+		protected AppUser doInBackground(Object... params) {
+			
+			if (params[0] instanceof String && params[1] instanceof char[]) {
+				username = params[0].toString();
+				password = (char[]) params[1];
+			}
+			
+			AppUser currentUser = serviceClient.loginUser(username, password);
+			if (currentUser != null) {
+				return currentUser;
+			} else {
+				// TODO: register the new account here.
+				return null;
+			}
+		}
+		
+		@Override
+		protected void onPostExecute(final AppUser user) {
+			if (callbacks != null) {
+				callbacks.onPostExecute(user);
+			}
+		}
+		
+		
+		@Override
+		protected void onCancelled() {
+			if (callbacks != null) {
+				callbacks.onCancelled();
+			}
+		}
+	}
+}
